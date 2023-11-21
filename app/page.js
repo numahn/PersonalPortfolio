f8b630c3877c838745deec96fa5c09559203f55a
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-21 18:21:08 -0500

    added pfp, animation works without moving everything

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 import styles from './page.module.css'
 import Image from 'next/image'
+import mfw from "@/public/mfw.png"
 export default function Home() {
   return (
    <>
       <div className={styles.container}>
         <div className={styles.titleCont}>
-          <p className={styles.hello}>Hey, I'm Noman.</p>
+          <div className={styles.animationCont}>
+            <p className={styles.hello}>Hey, I'm Noman.</p>
+          </div>
+          <Image className={styles.profilePic} src={mfw.src} alt="profilepic" width={300} height={300}></Image>
         </div>
-        <Image className={styles.profilePic} src="/mfw.png" alt="profilepic" width={300} height={300}></Image>
         
       </div>
    </>
