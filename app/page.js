aba53e26f5acae6a4268a04cd7c04c8f970817c4
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-22 19:03:35 -0500

    testing idea

--- app/page.js
+++ app/page.js
@@ -11,8 +11,13 @@ export default function Home() {
           </div>
           <Image className={styles.profilePic} src={mfw.src} alt="profilepic" width={350} height={350}></Image>
         </div>
-        <p className={`${styles.text} ${styles.soft}`}>Software Engineer &<br></br>Network Engineer</p>
-        <p classname={styles.about}>ASDFJAHFJADHFGJDAHGJDAHGTJADHGJADHGJADHJHGAD</p>
+        <p className={`${styles.text} ${styles.soft} ${styles.blur}`}>Software Engineer &<br></br>Network Engineer</p>
+        <div className={styles.aboutCont}>
+          <div className={styles.test2}>
+            <div className={styles.test}></div>
+            <p className={styles.about}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
       </div>
    </>
   )
