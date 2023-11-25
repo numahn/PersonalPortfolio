336ce3036a22cb85dd90a60641a5ba5472ed47d3
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-24 23:06:45 -0500

    removed unnessary code, working on project cards

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,18 +1,14 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 export default function Page() {
   return (
     <>
       <div className={styles.container}>
-        <p style={{fontSize: '36px'}}>Projects</p>
+        <p style={{ fontSize: "36px" }}>Projects</p>
         <div className={styles.projTable}>
-          <div className={styles.project}>
-            Project 1
-          </div>
-          <div className={styles.project}>
-            Project 2
-          </div>
+          <div className={styles.project}>Project 1</div>
+          <div className={styles.project}>Project 2</div>
         </div>
       </div>
     </>
-  )
+  );
 }
