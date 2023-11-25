8e27da6f3cce14c3e6a26db7af8f564014cd423a
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-24 23:00:32 -0500

    init projects branch, formatting page

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,7 +2,17 @@ import styles from './page.module.css'
 export default function Page() {
   return (
     <>
-      Projects page here!
+      <div className={styles.container}>
+        <p style={{fontSize: '36px'}}>Projects</p>
+        <div className={styles.projTable}>
+          <div className={styles.project}>
+            Project 1
+          </div>
+          <div className={styles.project}>
+            Project 2
+          </div>
+        </div>
+      </div>
     </>
   )
 }
