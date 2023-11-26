7961ac1a1252f630ebe2d5893dd30bd34c1b99b6
Noman <80441451+nali556@users.noreply.github.com>
2023-11-25 21:24:24 -0500

    adjustments to make animations and ui work with more screens

--- app/projects/page.js
+++ app/projects/page.js
@@ -5,10 +5,10 @@ export default function Page() {
       <div className={styles.container}>
         <p className={styles.title}>Projects</p>
         <div className={styles.projTable}>
-          <div className={styles.project}>Project 1</div>
-          <div className={styles.project2}>Project 2</div>
-          <div className={styles.project}>Project 2</div>
-          <div className={styles.project2}>Project 2</div>
+          <div className={`${styles.project} ${styles.pr1}`}>Project 1</div>
+          <div className={`${styles.pr2} ${styles.project}`}>Project 2</div>
+          <div className={`${styles.project} ${styles.pr1}`}>Project 2</div>
+          {/* <div className={styles.project2}>Project 2</div> */}
           {/* <div className={styles.project}>Project 2</div>
           <div className={styles.project}>Project 2</div> */}
           {/* <div className={styles.project}>Project 2</div>
