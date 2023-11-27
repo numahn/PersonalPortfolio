fc0ed6ddf4cadcf48ed3d711e5168f18b18cdbf1
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-27 17:48:58 -0500

    added images for projects, need python project image

--- app/projects/page.js
+++ app/projects/page.js
@@ -5,9 +5,9 @@ export default function Page() {
       <div className={styles.container}>
         <p className={styles.title}>Projects</p>
         <div className={styles.projTable}>
-          <div className={`${styles.project} ${styles.pr1}`}>Project 1</div>
-          <div className={`${styles.pr2} ${styles.project}`}>Project 2</div>
-          <div className={`${styles.project} ${styles.pr1}`}>Project 3</div>
+          <div className={`${styles.project} ${styles.pr1} ${styles.first}`}>Project 1</div>
+          <div className={`${styles.pr2} ${styles.project} ${styles.second}`}>Project 2</div>
+          <div className={`${styles.project} ${styles.pr1} ${styles.third}`}>Project 3</div>
           {/* <div className={styles.project}>Project 2</div>
           <div className={styles.project}>Project 2</div> */}
           {/* <div className={styles.project}>Project 2</div>
