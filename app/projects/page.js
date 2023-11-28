02f81768f6bd6ac2af85f9fdd361e5ebc383731b
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-27 21:10:12 -0500

    animations for all projects, project 2 need adjustment

--- app/projects/page.js
+++ app/projects/page.js
@@ -9,8 +9,16 @@ export default function Page() {
             <div className={`${styles.project} ${styles.pr1} ${styles.first}`}></div>
             <p className={`${styles.desc1} ${styles.hidden}`} disabled>asdf</p>
           </div>
-          <div className={`${styles.pr2} ${styles.project} ${styles.second}`}>Project 2</div>
-          <div className={`${styles.project} ${styles.pr1} ${styles.third}`}>Project 3</div>
+          <div className={styles.projHolder}>
+            <div className={`${styles.project} ${styles.pr2} ${styles.second}`}></div>
+            <p className={`${styles.desc1} ${styles.hidden}`} disabled>asdf</p>
+          </div>
+          <div className={styles.projHolder}>
+            <div className={`${styles.project} ${styles.pr1} ${styles.first}`}></div>
+            <p className={`${styles.desc1} ${styles.hidden}`} disabled>asdf</p>
+          </div>
+          {/* <div className={`${styles.pr2} ${styles.project} ${styles.second}`}>Project 2</div> */}
+          {/* <div className={`${styles.project} ${styles.pr1} ${styles.third}`}>Project 3</div> */}
           {/* <div className={styles.project}>Project 2</div>
           <div className={styles.project}>Project 2</div> */}
           {/* <div className={styles.project}>Project 2</div>
