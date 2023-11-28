90a46efd8a12c10c7c31aa3b7d2b480bf7bc3843
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-27 21:14:48 -0500

    quick fix for proj2 animation

--- app/projects/page.js
+++ app/projects/page.js
@@ -6,23 +6,29 @@ export default function Page() {
         <p className={styles.title}>Projects</p>
         <div className={styles.projTable}>
           <div className={styles.projHolder}>
-            <div className={`${styles.project} ${styles.pr1} ${styles.first}`}></div>
-            <p className={`${styles.desc1} ${styles.hidden}`} disabled>asdf</p>
+            <div
+              className={`${styles.project} ${styles.pr1} ${styles.first}`}
+            ></div>
+            <p className={styles.desc} disabled>
+              asdf
+            </p>
           </div>
           <div className={styles.projHolder}>
-            <div className={`${styles.project} ${styles.pr2} ${styles.second}`}></div>
-            <p className={`${styles.desc1} ${styles.hidden}`} disabled>asdf</p>
+            <div
+              className={`${styles.project} ${styles.pr2} ${styles.second}`}
+            ></div>
+            <p className={styles.desc2} disabled>
+              asdf
+            </p>
           </div>
           <div className={styles.projHolder}>
-            <div className={`${styles.project} ${styles.pr1} ${styles.first}`}></div>
-            <p className={`${styles.desc1} ${styles.hidden}`} disabled>asdf</p>
+            <div
+              className={`${styles.project} ${styles.pr1} ${styles.first}`}
+            ></div>
+            <p className={styles.desc} disabled>
+              asdf
+            </p>
           </div>
-          {/* <div className={`${styles.pr2} ${styles.project} ${styles.second}`}>Project 2</div> */}
-          {/* <div className={`${styles.project} ${styles.pr1} ${styles.third}`}>Project 3</div> */}
-          {/* <div className={styles.project}>Project 2</div>
-          <div className={styles.project}>Project 2</div> */}
-          {/* <div className={styles.project}>Project 2</div>
-          <div className={styles.project}>Project 2</div> */}
         </div>
       </div>
     </>
