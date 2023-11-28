4822a468814e7d96cc9f1add7e8a710d2ed472a5
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-27 20:57:50 -0500

    animation card slides down

--- app/projects/page.js
+++ app/projects/page.js
@@ -5,7 +5,10 @@ export default function Page() {
       <div className={styles.container}>
         <p className={styles.title}>Projects</p>
         <div className={styles.projTable}>
-          <div className={`${styles.project} ${styles.pr1} ${styles.first}`}>Project 1</div>
+          <div className={styles.projHolder}>
+            <div className={`${styles.project} ${styles.pr1} ${styles.first}`}></div>
+            <p className={`${styles.desc1} ${styles.hidden}`} disabled>asdf</p>
+          </div>
           <div className={`${styles.pr2} ${styles.project} ${styles.second}`}>Project 2</div>
           <div className={`${styles.project} ${styles.pr1} ${styles.third}`}>Project 3</div>
           {/* <div className={styles.project}>Project 2</div>
