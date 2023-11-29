f41c8b25d45561d776d774a4ca9b3aa73d8dea7b
Noman Ali <80441451+numahn@users.noreply.github.com>
2023-11-28 21:23:53 -0500

    removed github icon on projects, made github link background of each card

--- app/projects/page.js
+++ app/projects/page.js
@@ -10,7 +10,7 @@ export default function Page() {
         <p className={styles.title}>Projects</p>
         <div className={styles.projTable}>
           <div className={styles.projHolder}>
-            <Link href="https://ggc-numahn.vercel.app">
+            <Link href="https://github.com/numahn/NBAGatchaFrontend">
               <div
                 className={`${styles.project} ${styles.pr1} ${styles.first}`}
               ></div>
@@ -21,64 +21,39 @@ export default function Page() {
                 Create an account and start opening up cards of your favorite
                 NBA players to showcase!
               </p>
-              <div className={styles.stack}>
-                <p>PostgreSQL, React, Express.js, Node.js</p>
-                <Link className={styles.descIcon} href="https://github.com/numahn/NBAGatchaFrontend">
-                  <Image
-                    src={githubicon.src}
-                    alt="github"
-                    width={20}
-                    height={20}
-                  ></Image>
-                </Link>
-              </div>
+              <p>Used PostgreSQL, React, Express.js, Node.js</p>
             </div>
           </div>
           <div className={styles.projHolder}>
-            <div
-              className={`${styles.project} ${styles.pr2} ${styles.second}`}
-            ></div>
+            <Link href="https://github.com/numahn/TechNotesBackendJS">
+              <div
+                className={`${styles.project} ${styles.pr2} ${styles.second}`}
+              ></div>
+            </Link>
             <div className={styles.desc2}>
               <p style={{ paddingBottom: ".5rem" }}>TechNotes</p>
               <p style={{ paddingBottom: ".5rem" }}>Have your own personal cloud storage of note taking / photos at the
                 tip of your fingers!
               </p>
-              <div className={styles.stack}>
-                <p>Used React, Node.js, Express.js, MySQL</p>
-                <Link className={styles.descIcon} href="https://github.com/numahn/TechNotesBackendJS">
-                  <Image
-                    src={githubicon.src}
-                    alt="github"
-                    width={20}
-                    height={20}
-                  ></Image>
-                </Link>
-              </div>
+              <p>Used React, Node.js, Express.js, MySQL</p>
             </div>
           </div>
           <div className={styles.projHolder}>
-            <div
-              className={`${styles.project} ${styles.pr1} ${styles.first}`}
-            ></div>
+            <Link href="https://github.com/numahn/python-gpt">
+              <div
+                className={`${styles.project} ${styles.pr1} ${styles.first}`}
+              ></div>
+            </Link>
             <div className={styles.desc}>
               <p style={{ paddingBottom: ".5rem" }}>OpenAI Chatbot</p>
               <p style={{ paddingBottom: ".5rem" }}>
                 Have your very own AI assistant awaiting and answering your every question!
               </p>
-              <div className={styles.stack}>
-                <p>Used Python, Django, HTML, CSS, JavaScript, SQLite</p>
-                <Link className={styles.descIcon} href="https://github.com/numahn/python-gpt">
-                  <Image
-                    src={githubicon.src}
-                    alt="github"
-                    width={20}
-                    height={20}
-                  ></Image>
-                </Link>
-              </div>
+              <p>Used Python, Django, HTML, CSS, JavaScript, SQLite</p>
             </div>
           </div>
         </div>
+        
       </div>
     </>
   );
